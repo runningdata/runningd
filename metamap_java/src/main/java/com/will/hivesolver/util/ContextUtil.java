9cc3ae1089e63508a663edd21cf63befe4bef012
WillCup <willcup@163.com>
2016-07-15 18:56:50 +0800

    modify execution status

--- metamap_java/src/main/java/com/will/hivesolver/util/ContextUtil.java
+++ metamap_java/src/main/java/com/will/hivesolver/util/ContextUtil.java
@@ -3,7 +3,9 @@ package com.will.hivesolver.util;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ContextUtil implements ApplicationContextAware {
 
     private static ApplicationContext context;
