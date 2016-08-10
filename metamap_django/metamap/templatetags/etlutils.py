73b732e1fa75460da300aa6a9eb8967922874a5e
WillCup <willcup@163.com>
2016-08-10 15:03:39 +0800

    add datetime utils

--- metamap_django/metamap/templatetags/etlutils.py
+++ metamap_django/metamap/templatetags/etlutils.py
@@ -5,18 +5,50 @@ created by will
 '''
 import datetime
 from django import template
-from django.template import Context, Template
 
 register = template.Library()
 
 @register.simple_tag
-def current_time(format_string):
-    return datetime.datetime.now().strftime(format_string)
+def now_datetime():
+    return datetime.datetime.now().strftime('%Y%m%d%I%M%S')
+
+@register.simple_tag
+def now_datekey():
+    return datetime.datetime.now().strftime('%Y%m%d')
+
+@register.simple_tag
+def now_date():
+    return datetime.datetime.now().strftime('%Y-%m-%d')
+
+@register.simple_tag
+def date2datekey(date):
+    return datetime.datetime.strptime(date, '%Y-%m-%d').strftime('%Y%m%d')
+
+@register.simple_tag
+def datekey2date(datekey):
+    return datetime.datetime.strptime(datekey, '%Y%m%d').strftime('%Y-%m-%d')
+
+@register.simple_tag
+def now_datekey_add(num):
+    d = datetime.datetime.now() + datetime.timedelta(days=num)
+    return d.strftime('%Y%m%d')
+
+@register.simple_tag
+def now_date_add(num):
+    d = datetime.datetime.now() + datetime.timedelta(days=num)
+    return d.strftime('%Y-%m-%d')
 
 
-if __name__ == '__main__':
-    str = '{% load etlutils %}'
-    str += '\n {% current_time "%Y-%m-%d %I:%M %p" as nnn %}'
-    str += '\n I got a var : {{ nnn }}'
-    template = Template(str)
-    template.render(Context())
+@register.simple_tag
+def current_datekey_add(datekey, num):
+    '''
+    传入datekey和要添加的天数
+    '''
+    d = datetime.datetime.strptime(datekey, '%Y%m%d') + datetime.timedelta(days=num)
+    return d.strftime('%Y%m%d')
+
+@register.simple_tag
+def current_date_add(date, num):
+    d = datetime.datetime.strptime(date, '%Y-%m-%d') + datetime.timedelta(days=num)
+    return d.strftime('%Y-%m-%d')
+
