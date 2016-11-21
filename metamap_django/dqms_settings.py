a36456e44a05f947f69c152de71a9954473af608
WillCup <willcup@163.com>
2016-11-21 11:55:30 +0800

    start gunicorn

---
+++ metamap_django/dqms_settings.py
@@ -0,0 +1,23 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*
+'''
+created by will
+'''
+
+import  os
+
+print('gunicorn config is running....')
+
+bind = "0.0.0.0:9099"
+worker_class = "eventlet"
+workers = 4
+# workers = multiprocessing.cpu_count() * 2 + 1
+pidfile = '/tmp/dqms_gunicorn.pid'
+# accesslog = '/tmp/gunicorn_access.log'
+errorlog = '/tmp/dqms_gunicorn_error.log'
+loglevel = 'info'
+capture_output = True
+proc_name = 'will\'s dqms'
+daemon = True
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dqms.config.prod")
