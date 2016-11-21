b3914f8884432ab3e8f55476f8412bd167ed24d6
WillCup <willcup@163.com>
2016-11-21 15:56:32 +0800

    disable cas

--- metamap_django/dqms/urls.py
+++ metamap_django/dqms/urls.py
@@ -20,28 +20,28 @@ router.register(r'execforcheck', CheckInstViewSet)
 router.register(r'execforcase', CaseInstViewSet)
 
 urlpatterns = [
-    # url(r'^$', login_required(case.IndexView.as_view()), name='index'),
+    # url(r'^$', case.IndexView.as_view()), name='index'),
 
     url(r'^$', case.IndexView.as_view(), name='index'),
-    url(r'^check/execs$', login_required(check.execution), name='check_execution'),
-    url(r'^check/$', login_required(check.manager), name='check_manager'),
-    url(r'^check/del/$', login_required(check.delete), name='check_delete'),
-    url(r'^check/edit/$', login_required(check.edit), name='check_edit'),
-    url(r'^check/save/$', login_required(check.save), name='check_save'),
-    url(r'^check/execution/(?P<pk>[0-9]+)/$', login_required(check.executions), name='check_execution'),
+    url(r'^check/execs$', check.execution, name='check_execution'),
+    url(r'^check/$', check.manager, name='check_manager'),
+    url(r'^check/del/$', check.delete, name='check_delete'),
+    url(r'^check/edit/$', check.edit, name='check_edit'),
+    url(r'^check/save/$', check.save, name='check_save'),
+    url(r'^check/execution/(?P<pk>[0-9]+)/$', check.executions, name='check_execution'),
 
-    url(r'^alarm_info/$', login_required(alert.AlertView.as_view()), name='alarm_info'),
+    url(r'^alarm_info/$', alert.AlertView.as_view(), name='alarm_info'),
 
-    url(r'^case/execs$', login_required(case.execution), name='case_execution'),
+    url(r'^case/execs$', case.execution, name='case_execution'),
     # url(r'^case/$', case.manager, name='case_manager'),
-    url(r'^case/$', login_required(case.CaseView.as_view()), name='case_manager'),
-    url(r'^case/edit/$', login_required(case.edit), name='case_edit'),
-    url(r'^case/save/$', login_required(case.save), name='case_save'),
-    url(r'^case/del/$', login_required(case.delete), name='case_delete'),
-    url(r'^case/runtest/(?P<pk>[0-9]+)/$', login_required(case.runtest), name='case_runtest'),
+    url(r'^case/$', case.CaseView.as_view(), name='case_manager'),
+    url(r'^case/edit/$', case.edit, name='case_edit'),
+    url(r'^case/save/$', case.save, name='case_save'),
+    url(r'^case/del/$', case.delete, name='case_delete'),
+    url(r'^case/runtest/(?P<pk>[0-9]+)/$', case.runtest, name='case_runtest'),
 
 
-    url(r'^test/$', login_required(case.test), name='test'),
+    url(r'^test/$', case.test, name='test'),
 
 
 
