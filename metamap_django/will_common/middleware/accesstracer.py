551c5eb8ef1245e96d71490db17cf22249a9b60c
WillCup <willcup@163.com>
2016-11-22 18:07:30 +0800

    add post and get params for access log

--- metamap_django/will_common/middleware/accesstracer.py
+++ metamap_django/will_common/middleware/accesstracer.py
@@ -11,6 +11,5 @@ logger = logging.getLogger('error')
 
 class AccessTracer():
     def process_request(self, request):
-        logger.info('user : %s -> url : %s' % (request.user.username, request.path))
-        print('user : %s -> url : %s' % (request.user.username, request.path))
+        print('user : %s -> url : %s . post params : %s , get params : %s' % (request.user.username, request.path, dict(request.POST), dict(request.GET)))
         return None
