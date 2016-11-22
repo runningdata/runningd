b7eee8fa8e2c46ecb17e8093a432decd24bb6846
WillCup <willcup@163.com>
2016-11-22 16:10:14 +0800

    add will_common for metamap

--- metamap_django/metamap/config/prod.py
+++ metamap_django/metamap/config/prod.py
@@ -81,23 +81,15 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE_CLASSES = [
-    # 'will_common.middleware.viewexception.ViewException',
+    'will_common.middleware.viewexception.ViewException',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'django.middleware.security.SecurityMiddleware',
-    # 'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'django.middleware.common.CommonMiddleware',
-    # # 'django.middleware.csrf.CsrfViewMiddleware',
-    # # 'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    # 'django.contrib.messages.middleware.MessageMiddleware',
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'metamap_django.metamap_urls'
