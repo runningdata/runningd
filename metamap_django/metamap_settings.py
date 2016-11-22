f6012e4d57867e314fa381069855b2c821755f01
WillCup <willcup@163.com>
2016-11-22 15:48:28 +0800

    add dqms_setting

--- metamap_django/metamap_settings.py
+++ metamap_django/metamap_settings.py
@@ -12,12 +12,12 @@ bind = "0.0.0.0:9099"
 worker_class = "eventlet"
 workers = 4
 # workers = multiprocessing.cpu_count() * 2 + 1
-pidfile = '/tmp/dqms_gunicorn.pid'
+pidfile = '/tmp/metamap_gunicorn.pid'
 # accesslog = '/tmp/gunicorn_access.log'
-errorlog = '/tmp/dqms_gunicorn_error.log'
+errorlog = '/tmp/metamap_gunicorn_error.log'
 loglevel = 'info'
 capture_output = True
-proc_name = 'will\'s dqms'
+proc_name = 'will\'s metamap'
 daemon = True
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dqms.config.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "metamap.config.prod")
