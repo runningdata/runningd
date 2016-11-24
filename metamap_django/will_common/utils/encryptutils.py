98041558ca0cc0689c827fba2d25624b77b1fc6b
WillCup <willcup@163.com>
2016-11-24 17:11:17 +0800

    fix lgon msg

--- metamap_django/will_common/utils/encryptutils.py
+++ metamap_django/will_common/utils/encryptutils.py
@@ -13,7 +13,7 @@ cc = AES.new(settings.PUSH_KEY)
 def encrpt_msg(msg):
     if type(msg) == 'unicode':
         msg_ = msg.encode("utf8")
-    else:
+    elif type(msg) == 'long' or type(msg) == 'int':
         msg_ = str(msg)
     return base64.b64encode(cc.encrypt(pad(msg)))
 
