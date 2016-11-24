6fcf2223156d2c115acc5a3ef67c4e6aa70c8813
WillCup <willcup@163.com>
2016-11-24 14:04:19 +0800

    tt

--- metamap_django/will_common/utils/PushUtils.py
+++ metamap_django/will_common/utils/PushUtils.py
@@ -2,6 +2,8 @@
 import logging
 import urllib2
 
+import requests
+
 from metamap_django import settings
 from will_common.utils.encryptutils import encrpt_msg
 
@@ -15,18 +17,33 @@ def push_msg(users, msg):
 
 
 def push_msg_tophone(phone, msg):
-    logging.info('alert happening: %s : %s' % (phone, msg))
+    # logging.info('alert happening: %s : %s' % (phone, msg))
+    # headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36"}
+    # r = requests.get(push_url % (encrpt_msg(phone), encrpt_msg(msg)), headers=headers)
+    # print r.headers
+    # print r.content
 
-    req = urllib2.Request(
-        push_url % (encrpt_msg(phone), encrpt_msg(msg)))
-    # req.add_header('User-Agent',
-    #                'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36')
-    try:
-        resp = urllib2.urlopen(req)
-        if resp.getcode() == 200:
-            print resp.read()
-            return 'push phone success'
-        else:
-            return 'error', resp.read()
-    except urllib2.URLError, e:
-        return e.reason
+    data = {'mobileNo': encrpt_msg('18811395620'), 'content': encrpt_msg('hehe')}
+    header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'}
+
+    rep = requests.get(url='https://advert.jianlc.com/sendMessage.shtml', data=data, headers=header)
+    print rep.content
+
+    # req = urllib2.Request(
+    #     push_url % (encrpt_msg(phone), encrpt_msg(msg)), headers=headers)
+    # req.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+    # req.add_header('Accept-Encoding', 'gzip, deflate, sdch, br')
+    # req.add_header('Upgrade-Insecure-Requests', 1)
+    # # req.add_header('User-Agent',
+    # #                'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36')
+    # print('header : %s' % req.get_header('User-Agent'))
+    # try:
+    #     resp = urllib2.urlopen(req)
+    #     if resp.getcode() == 200:
+    #         print resp.read()
+    #         return 'push phone success'
+    #     else:
+    #         return 'error', resp.read()
+    # except urllib2.URLError, e:
+    #     return e.reason
