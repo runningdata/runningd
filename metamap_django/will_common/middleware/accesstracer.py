b465ca573ea11ef6c680e90e7d98306e22f029a6
WillCup <willcup@163.com>
2016-11-25 10:54:44 +0800

    add time for access log

--- metamap_django/will_common/middleware/accesstracer.py
+++ metamap_django/will_common/middleware/accesstracer.py
@@ -6,10 +6,12 @@ created by will
 import logging
 import traceback
 
+from will_common.utils import dateutils
+
 logger = logging.getLogger('error')
 
 
 class AccessTracer():
     def process_request(self, request):
-        print('user : %s -> url : %s . post params : %s , get params : %s' % (request.user.username, request.path, dict(request.POST), dict(request.GET)))
+        print('%s []: user : %s -> url : %s . post params : %s , get params : %s' % (dateutils.now_datetime(), request.user.username, request.path, dict(request.POST), dict(request.GET)))
         return None
