f370d8e203d8d1413df5f2cc5a38ae7a56c3a145
WillCup <willcup@163.com>
2017-01-12 12:25:18 +0800

    fix sqoop

--- metamap_django/metamap/helpers/etlhelper.py
+++ metamap_django/metamap/helpers/etlhelper.py
@@ -280,9 +280,12 @@ def generate_job_file_h2m(objs, folder):
         job_name = obj.name
         task = SqoopHive2Mysql.objects.get(pk=obj.rel_id)
         command = generate_sqoop_hive2mysql(task, schedule=obj.schedule).replace('&', '\&')
+        sqoop_file = AZKABAN_SCRIPT_LOCATION + folder + "-" + job_name + ".h2m"
+        with open(sqoop_file, 'w') as f:
+            f.write(command)
         # 生成job文件
-        job_type = 'command'
-        content = '#' + job_name + '\n' + 'type=' + job_type + '\n' + 'command = ' + command + '\n'
+        job_type = 'command\n retries=12\n retry.backoff=300000\n'
+        content = '#' + job_name + '\n' + 'type=' + job_type + '\n' + 'command = bash -X ' + sqoop_file + '\n'
         job_file = AZKABAN_BASE_LOCATION + folder + "/" + job_name + ".job"
         with open(job_file, 'w') as f:
             f.write(content)
@@ -300,9 +303,12 @@ def generate_job_file_m2h(objs, folder):
         job_name = obj.name
         task = SqoopMysql2Hive.objects.get(pk=obj.rel_id)
         command = generate_sqoop_mysql2hive(task, schedule=obj.schedule).replace('&', '\&')
+        sqoop_file = AZKABAN_SCRIPT_LOCATION + folder + "-" + job_name + ".m2h"
+        with open(sqoop_file, 'w') as f:
+            f.write(command)
         # 生成job文件
         job_type = ' command \n retries=12\n retry.backoff=300000\n'
-        content = '#' + job_name + '\n' + 'type=' + job_type + '\n' + 'command = ' + command
+        content = '#' + job_name + '\n' + 'type=' + job_type + '\n' + 'command = bash -X ' + sqoop_file + '\n'
         job_file = AZKABAN_BASE_LOCATION + folder + "/" + job_name + ".job"
         with open(job_file, 'w') as f:
             f.write(content)
