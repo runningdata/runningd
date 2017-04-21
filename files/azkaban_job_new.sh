8587303c7b1e0c52b25afb4eaac32bd36d72a622
WillCup <willcup@163.com>
2017-04-22 00:13:34 +0800

    ip

--- files/azkaban_job_new.sh
+++ files/azkaban_job_new.sh
@@ -26,8 +26,8 @@ else
 fi
 
 tmp_output=/tmp/${job_type}_${prefix}_tmp_log
-host=10.0.1.62:8081
-metamap_host=10.0.1.62:8088
+host=10.2.19.62:8081
+metamap_host=10.2.19.62:8088
 project_desc=${prefix}_schedule
 
 # 调用生成job的任务，返回任务名称或者失败信息
--- metamap_django/will_common/models.py
+++ metamap_django/will_common/models.py
@@ -19,6 +19,12 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+class CeleryTask():
+    def __init__(self, queue, command, name):
+        self.queue = queue
+        self.command = command
+        self.name = name
+
 class WillDependencyTask(models.Model):
     name = models.CharField(unique=True, max_length=200)
     schedule = models.IntegerField(null=False, default=1)  # 0 天 1 周 2 月 3 季度 4 cron
