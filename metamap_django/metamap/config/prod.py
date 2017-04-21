aa4594c5ca90504096ab06d2fb4d884c8a2dc36e
WillCup <willcup@163.com>
2017-04-21 17:34:23 +0800

    specified session id

--- metamap_django/metamap/config/prod.py
+++ metamap_django/metamap/config/prod.py
@@ -29,6 +29,7 @@ SECRET_KEY = 'nyps=8t#p69#1a$be^m^)c$_3k^*7aldic%p(8jnzh=@wcbk1w'
 DEBUG = False
 SESSION_COOKIE_NAME = 'xsid'
 CSRF_COOKIE_NAME = 'xcsrftoken'
+SESSION_COOKIE_PATH = 'mmp'
 
 # email settings
 EMAIL_HOST = 'smtp.exmail.qq.com'
