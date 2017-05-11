bab402c0f2fca92cb3c2c03fe308ad1341ccd2a4
WillCup <willcup@163.com>
2017-05-11 17:13:58 +0800

    fix

--- metamap_django/will_common/middleware/accesstracer.py
+++ metamap_django/will_common/middleware/accesstracer.py
@@ -25,10 +25,11 @@ class AccessTracer():
 class AuthTracer():
 
     def process_request(self, request):
-        is_gene = request.path.startswith(
-            '/metamap') and '/rest/' not in request.path and '/generate_job_dag/' not in request.path
+        is_filelist = request.path.startswith(
+            'metamap/rest/exports')
+        is_gene = '/generate_job_dag/' in request.path
         for k, v in settings.PATH_AUTH_DICT.items():
-            if not request.user.has_perm(k) and v in request.path and not is_gene:
+            if not request.user.has_perm(k) and v in request.path and not is_gene and not is_filelist:
                 message = u'您没有访问此路径的权限'
                 return render(request, 'common/message.html', {'message': message})
         return None
