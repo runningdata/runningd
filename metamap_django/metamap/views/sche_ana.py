0d8825f952fd732df59ea4e0c186d96a5e055b27
WillCup <willcup@163.com>
2017-06-23 17:36:27 +0800

    ss

--- metamap_django/metamap/views/sche_ana.py
+++ metamap_django/metamap/views/sche_ana.py
@@ -171,7 +171,7 @@ class ExportsViewSet(viewsets.ModelViewSet):
             objs = Exports.objects.filter(start_time__gt=days).order_by('-start_time')
             result = list()
             for export in objs:
-                if export.task:
+                if WillDependencyTask.objects.filter(pk=export.task_id).count() == 1:
                     if export.task.type == 2:
                         ana_id = export.task.rel_id
                     elif export.task.type == 100:
