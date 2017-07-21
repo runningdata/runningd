c4c1465a356b6da9dd8bfb6caa589b589bed8ca2
WillCup <willcup@163.com>
2017-07-21 14:31:02 +0800

    new redis broker

--- metamap_django/metamap/config/prod.py
+++ metamap_django/metamap/config/prod.py
@@ -82,7 +82,8 @@ djcelery.setup_loader()
 # Celery Beat 设置
 CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 CELERYD_TASK_TIME_LIMIT = 3600
-BROKER_URL = 'redis://datanode08.yinker.com:6379'
+# BROKER_URL = 'redis://datanode08.yinker.com:6379'
+BROKER_URL = 'redis://10.2.19.113:6480'
 
 CELERY_ROUTES = {
     'metamap.tasks.exec_jar': {
@@ -90,8 +91,10 @@ CELERY_ROUTES = {
     },
 }
 
-CELERY_REDIS_HOST = 'datanode08.yinker.com'
-CELERY_REDIS_PORT = '6379'
+CELERY_REDIS_HOST = '10.2.19.113'
+CELERY_REDIS_PORT = '6480'
+#CELERY_REDIS_HOST = 'datanode08.yinker.com'
+# CELERY_REDIS_PORT = '6379'
 # CELERY_TASK_SERIALIZER = 'json'
 # CELERY_ACCEPT_CONTENT = ['application/json']
 # CELERY_RESULT_SERIALIZER = 'json'
