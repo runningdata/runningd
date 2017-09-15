f0b6b8e25d8405f6dad3d56f7c42426e095f2403
WillCup <willcup@163.com>
2017-09-15 10:57:19 +0800

    init

---
+++ metamap_django/alert_settings.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*
+'''
+created by will
+'''
+
+import os
+
+print('running_alert gunicorn config is running....')
+
+bind = "0.0.0.0:10099"
+worker_class = "eventlet"
+workers = 2
+# workers = multiprocessing.cpu_count() * 2 + 1
+pidfile = '/tmp/running_alert_gunicorn.pid'
+# accesslog = '/tmp/gunicorn_access.log'
+errorlog = '/tmp/running_alert_gunicorn_error.log'
+loglevel = 'info'
+capture_output = True
+timeout=600
+proc_name = 'will\'s running_alert'
+daemon = True
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "running_alert.config.prod")
---
+++ metamap_django/bin/restart_alert.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+
+if [ -z $METAMAP_HOME ];then
+    echo "Please set env $METAMAP_HOME first."
+    exit 1
+else
+    echo "METAMAP_HOME is : $METAMAP_HOME"
+fi
+
+## 检查当前进程中是否还有dqms的gunicorn进程活着
+function check_gunicorn() {
+    lines=`ps -ef | grep alert_settings | grep -c  /server/xstorm/bin/gunicorn`
+    if [[ $lines > 1 ]]; then
+        echo "${lines} gunicorns still running..."
+        ps -ef |grep alert_settings
+        return ${lines}
+    else
+        echo "All gunicorns has been killed"
+        return 0
+    fi
+}
+
+
+#################################
+###  1. pull最新代码           ####
+#################################
+
+cd $METAMAP_HOME \
+    && git pull
+
+#################################
+###  2. 停止所有gunicorn进程   ####
+#################################
+pid=`ps -ef | grep gunicorn | grep alert_settings | awk '{if($3 == '1') print $2}'`
+if [[ $pid > 0 ]]; then
+    echo "Got gunicorn master pid : ${pid}"
+    kill $pid
+    check_gunicorn
+    status=$?
+    sleep 10s
+    until [ $status -eq 0 ]
+    do
+        check_gunicorn
+        status=$?
+        sleep 5s
+    done
+else
+    echo "Cannot find master pid for gunicorn"
+fi
+
+
+######################################
+###  3. 使用virtualenv启动gunicorn ####
+######################################
+
+cd $METAMAP_HOME/metamap_django
+/server/xstorm/bin/python \
+    /server/xstorm/bin/gunicorn \
+    metamap_django.wsgi:application \
+     -c alert_settings.py
+echo "##########   log  ############"
+sleep 3s
+tail -20 /tmp/running_alert_gunicorn_error.log
