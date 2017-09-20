00e0403a87c36d5d63ca7ebddf8218cf24c30a90
WillCup <willcup@163.com>
2017-09-20 11:53:28 +0800

    ss

--- metamap_django/metamap/config/celery_conf.py
+++ metamap_django/metamap/config/celery_conf.py
@@ -13,11 +13,21 @@ CELERY_ROUTES = {
     'metamap.tasks.exec_jar': {
         'queue': 'running_jar',
     },
+    'running_alert.tasks.*': {
+        'queue': 'running_alert',
+    },
 }
+# CELERY_ROUTES = ([
+#                      {'metamap.tasks.exec_jar': {
+#                          'queue': 'running_jar',
+#                      }},
+#                      {'web.tasks.*', {'queue': 'web'}},
+#                  ],)
+
 CELERY_TIMEZONE = "Asia/Shanghai"
 CELERY_REDIS_HOST = '10.2.19.113'
 CELERY_REDIS_PORT = '6480'
-#CELERY_REDIS_HOST = 'datanode08.yinker.com'
+# CELERY_REDIS_HOST = 'datanode08.yinker.com'
 # CELERY_REDIS_PORT = '6379'
 # CELERY_TASK_SERIALIZER = 'json'
 # CELERY_ACCEPT_CONTENT = ['application/json']
