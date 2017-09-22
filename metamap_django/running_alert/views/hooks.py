15c62264c2d72fac8739ca1ebb2b818c69d25ad0
WillCup <willcup@163.com>
2017-09-22 12:01:27 +0800

    fix

--- metamap_django/running_alert/views/hooks.py
+++ metamap_django/running_alert/views/hooks.py
@@ -1,4 +1,5 @@
 import json
+import traceback
 
 from django.http import HttpResponse
 from running_alert.models import MonitorInstance, SparkMonitorInstance
@@ -8,24 +9,28 @@ from will_common.utils import PushUtils
 
 def alert_for_prome(request):
     if request.method == 'POST':
-        message = json.loads(request.body)
-        summary = message['commonAnnotations']['summary']
-        description = message['commonAnnotations']['description']
-        alertname = message['commonLabels']['alertname']
-        app_name = message['commonLabels']['app_name']
-        if 'spark' not in message['commonLabels']['service']:
-            obj = MonitorInstance.objects.get(host_and_port=message['commonLabels']['host_and_port'])
-        else:
-            obj = SparkMonitorInstance.objects.get(instance_name=message['commonLabels']['app_name'])
-        target_users = []
-        for manager in obj.managers.split(','):
-            target_users.append(UserProfile.objects.get(user__username=manager))
+        try:
+            message = json.loads(request.body)
+            summary = message['commonAnnotations']['summary']
+            description = message['commonAnnotations']['description']
+            alertname = message['commonLabels']['alertname']
+            app_name = message['commonLabels']['app_name']
+            if 'spark' not in message['commonLabels']['service']:
+                obj = MonitorInstance.objects.get(host_and_port=message['commonLabels']['host_and_port'])
+            else:
+                obj = SparkMonitorInstance.objects.get(instance_name=message['commonLabels']['app_name'])
+            target_users = []
+            for manager in obj.managers.split(','):
+                target_users.append(UserProfile.objects.get(user__username=manager))
 
-        p_msg = 'Alert for %s.%s. summary is : %s. description is : %s' % (
-            app_name, alertname, summary, description)
-        PushUtils.push_msg(target_users, p_msg)
+            p_msg = 'Alert for %s.%s. summary is : %s. description is : %s' % (
+                app_name, alertname, summary, description)
+            PushUtils.push_msg(target_users, p_msg)
 
-        PushUtils.push_msg_tophone('15210976096', p_msg)
-        return HttpResponse('Done')
+            PushUtils.push_msg_tophone('15210976096', p_msg)
+            return HttpResponse('Done')
+        except Exception, e:
+            print traceback.format_exc()
+            print message
     else:
         return HttpResponse('use POST please')
