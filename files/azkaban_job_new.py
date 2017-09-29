1dfb20cfecc1a6b8d705f769d9587971ec80d2c4
WillCup <willcup@163.com>
2017-09-29 11:04:06 +0800

    fix schedule

--- files/azkaban_job_new.py
+++ files/azkaban_job_new.py
@@ -1,13 +1,15 @@
-#!/usr/bin/python
-#coding:utf-8
+# -*- coding: utf-8 -*
+
+
 user = 'azkaban'
 pwd = '15yinker@bj'
 host = 'http://10.2.19.62:8081'
 metamap_host='10.2.19.62:8888'
 import urllib2,json, urllib
 
+from azkaban_client.azkaban import *
 
-import argparse
+import argparse, requests
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--group',dest='group', 
@@ -32,28 +34,32 @@ parser.add_argument('--flow_timeout',dest='flow_timeout',
                     type=int,
                     action='store',
                     default=210,
-                    help=u'单flow超时时间')
+                    help=u'timeout for a flow')
 args = parser.parse_args()
 
 
 def generate_task_files(jobtype, schedule, group):
-    target = 'http://10.2.19.62:8888/metamap/{job_type}/generate_job_dag/{num}/{group_name}/'.format(job_type=job_type, num=schedule, group_name=group)
+    target = 'http://10.2.19.62:8088/metamap/{job_type}/generate_job_dag/{num}/{group_name}/'.format(job_type=jobtype, num=schedule, group_name=group)
     resp = requests.get(target)
-    zip_file = '/tmp/{filename}.zip'.format(filename=resp.content)
+    return resp.content
+
+def get_file_name(proj):
+    zip_file = '/tmp/{filename}.zip'.format(filename=proj)
     print('Got job file %s' % zip_file)
     return zip_file
 
     
 def go_schedule(project, target_file):
-    from azkaban import *
     fetcher = CookiesFetcher(user, pwd)
     project = Project('WillTest', 'first test', fetcher)
     project.create_prj()
-    project.upload_zip('/tmp/h2h-20170923030038.zip')
+    project.upload_zip(target_file)
     for flow in project.fetch_flow():
         execution = flow.execute()
         execution.handle_timeout()
-    print('all end for %s ....' % project)
+    print('all end for %s ....' % project.name)
 
-
-generate_task_files(args.jobtype, args.schedule, args.group)
+print args
+prj_name = generate_task_files(args.jobtype, args.schedule, args.group)
+#prj_name = 'h2h-20170928161948'
+go_schedule(prj_name, get_file_name(prj_name))
