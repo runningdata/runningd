70b5ad815d3b1413ff304f8cc8a2a1b3438f4b07
WillCup <willcup@163.com>
2017-11-06 15:47:01 +0800

    adjsut

--- metamap_django/metamap/config/prod.py
+++ metamap_django/metamap/config/prod.py
@@ -63,8 +63,8 @@ HIVE_SERVER = {
 }
 
 # 设置cas服务器地址
-# CAS_SERVER_URL = "http://10.103.27.171:7000/sso/"
-CAS_SERVER_URL = "http://10.2.19.113:8181/sso/"
+CAS_SERVER_URL = "http://10.103.27.171:7000/sso/"
+CAS_IDC_SERVER_URL = "http://10.2.19.113:8181/sso/"
 # CAS_LOGOUT_COMPLETELY = True
 CAS_PROVIDE_URL_TO_LOGOUT = True
 # CAS_GATEWAY = True
