e858b881071e0b08e254de5f4c564a10d90ed0ed
WillCup <willcup@163.com>
2017-11-14 15:54:13 +0800

    mkdir

--- metamap_django/metamap/tasks.py
+++ metamap_django/metamap/tasks.py
@@ -345,6 +345,7 @@ def exec_will(task_id, **kwargs):
         # executions parallel.
         # jobs run parallel
         folder = 'schedule_flow_' + willtask.name + '_' + dateutils.now_datetime()
+        os.mkdir(folder)
         task_ids = [execobj.id for execobj in tasks]
         task_names = set()
         for execobj in tasks:
