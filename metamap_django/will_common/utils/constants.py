310f6b0a1f47704299bbc5b0a8375c53e057e61b
WillCup <willcup@163.com>
2017-12-07 16:13:50 +0800

    adjust alert info template

--- metamap_django/will_common/utils/constants.py
+++ metamap_django/will_common/utils/constants.py
@@ -12,7 +12,14 @@ MSG_NO_DATA = 'NO_DATA'
 DATASRC_TYPE_HIVE = 'hive'
 DATASRC_TYPE_MYSQL = 'mysql'
 DATASRC_TYPE_KYLIN = 'kylin'
-ALERT_MSG = u' %s 质检任务  质检用例  规则 %s   %s   %s 规则验证失败  期望值域 %d to %d 实际值 %d'
+ALERT_MSG = u'''
+        时间： %s
+        质检任务： %s
+        质检用例： %s
+        规则名称： %s
+        简述： %s 规则验证失败
+        期望值域： %d - %d
+        实际值 ： %d'''
 
 DATASRC_TYPE_MYSQL_DB = 'dqms_check'
 DATASRC_TYPE_KYLIN_DB = ''
