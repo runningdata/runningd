7274be8048fb30fcc64d3b177de555f5bf0ab149
WillCup <willcup@163.com>
2018-04-19 09:46:42 +0800

    debu

--- metamap_django/will_common/utils/redisutils.py
+++ metamap_django/will_common/utils/redisutils.py
@@ -14,7 +14,7 @@ pool = redis.ConnectionPool(host=settings.CELERY_REDIS_HOST, port=settings.CELER
 
 def get_keys():
     r = redis.StrictRedis(connection_pool=pool)
-    return [key for key in r.keys() if '_' not in key and 'unack' not in key]
+    return [key for key in r.keys() if '_' not in key and 'unack' not in key and 'countdown' not in key]
 
 
 def get_val(k, defaut='2000-01-01 12:12:12'):
