9e3fc0d47885175033778f8f9587e319c7bf6678
WillCup <willcup@163.com>
2018-05-09 18:02:57 +0800

    celery broker

--- metamap_django/running_alert/config/celery_conf.py
+++ metamap_django/running_alert/config/celery_conf.py
@@ -4,18 +4,25 @@ import djcelery
 djcelery.setup_loader()
 
 # Celery Beat 设置
+
+from init_config import result
+
+# Celery Beat 设置
 CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 CELERYD_TASK_SOFT_TIME_LIMIT = 3600
-BROKER_URL = 'redis://10.2.19.113:6480'
-CELERY_TIMEZONE = "Asia/Shanghai"
+BROKER_URL = 'redis://{celery_host}:{celery_port}'.format(celery_host=result.get('CELERY_REDIS_HOST', '10.2.19.113'),
+                                                          celery_port=result.get(['CELERY_REDIS_PORT'], '6480'))
+
 CELERY_ROUTES = {
     'running_alert.tasks.check_new_spark': {
         'queue': 'running_alert',
     },
 }
-
-CELERY_REDIS_HOST = '10.2.19.113'
-CELERY_REDIS_PORT = '6480'
+CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
+CELERY_TIMEZONE = "Asia/Shanghai"
+CELERY_REDIS_HOST = result.get('CELERY_REDIS_HOST', '10.2.19.113')
+CELERY_REDIS_PORT = result.get(['CELERY_REDIS_PORT'], '6480')
+CELERY_REDIS_PASSWORD = result.get(['CELERY_REDIS_PASSWORD'], '')
 # CELERY_REDIS_PORT = '6379'
 # CELERY_TASK_SERIALIZER = 'json'
 # CELERY_ACCEPT_CONTENT = ['application/json']
